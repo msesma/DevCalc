2c83ff06c34ed99b91b51f8a1416368315655ccc
Miguel Sesma <miguel.sesma@medtronic.com>
2022-09-23 16:37:59 +0200

    extract operands and operators

--- app/src/main/java/eu/sesma/devcalc/Solver.kt
+++ app/src/main/java/eu/sesma/devcalc/Solver.kt
@@ -2,6 +2,18 @@ package eu.sesma.devcalc
 
 class Solver {
     fun solve(noCursorOperation: String): String {
+        val operandStrings = noCursorOperation.split("+", "-", "x", "/")
+
+        val operators = (0 until operandStrings.size - 1).map { index ->
+            noCursorOperation.indexOfAny(
+                chars = "+-/x".toCharArray(),
+                startIndex = (0 until index).fold(
+                    initial = 0,
+                    operation = { acc, i -> acc + operandStrings[i].length }) + index
+            )
+        }.map { noCursorOperation[it] }
+        val operands = operandStrings.map { it.toFloat() }
+
         return noCursorOperation
     }
 }
