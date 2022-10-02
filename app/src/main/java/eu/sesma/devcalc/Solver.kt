61348bab0b7a1a0df7b98c87e89871f89668e7ee
Miguel Sesma <miguel@sesma.eu>
2022-10-02 17:30:09 +0200

    Start using TDD

--- app/src/main/java/eu/sesma/devcalc/Solver.kt
+++ app/src/main/java/eu/sesma/devcalc/Solver.kt
@@ -1,5 +1,6 @@
 package eu.sesma.devcalc
 
+import androidx.annotation.VisibleForTesting
 import eu.sesma.devcalc.Constants.ADD
 import eu.sesma.devcalc.Constants.DIV
 import eu.sesma.devcalc.Constants.MUL
@@ -7,19 +8,30 @@ import eu.sesma.devcalc.Constants.OPERANDS
 import eu.sesma.devcalc.Constants.SUB
 
 class Solver {
-    fun solve(noCursorOperation: String): String {
-        val operandStrings = noCursorOperation.split(ADD, SUB, MUL, DIV)
 
-        val operators = (0 until operandStrings.size - 1).map { index ->
-            noCursorOperation.indexOfAny(
+    fun solve(operation: String): String {
+        val operands = getOperands(operation)
+        val operators = getOperators(operation)
+
+        return operation
+    }
+
+    @VisibleForTesting()
+    internal fun getOperands(operation: String):List<Float> {
+        val operandStrings = operation.split(ADD, SUB, MUL, DIV)
+        return operandStrings.map { it.toFloat() }
+    }
+
+    @VisibleForTesting
+    internal fun getOperators(operation: String): List<Char> {
+        val operandStrings = operation.split(ADD, SUB, MUL, DIV)
+        return (0 until operandStrings.size - 1).map { index ->
+            operation.indexOfAny(
                 chars = OPERANDS.toCharArray(),
                 startIndex = (0 until index).fold(
                     initial = 0,
                     operation = { acc, i -> acc + operandStrings[i].length }) + index
             )
-        }.map { noCursorOperation[it] }
-        val operands = operandStrings.map { it.toFloat() }
-
-        return noCursorOperation
+        }.map { operation[it] }
     }
 }
